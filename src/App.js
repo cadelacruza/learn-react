621231f0ed313605d4dbbdeaabd1a1886562b9a3
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-05-27 13:59:30 -0500

    some

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Take out the ",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Take out ",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
